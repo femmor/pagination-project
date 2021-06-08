4623310ee0d1bf6d0753608dee50e2a32892978f
Emmanuel Egomson <fegomson@gmail.com>
2021-06-08 17:36:59 -0500

    initial-commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react'
+import { useFetch } from './useFetch'
+import Loader from "react-loading"
+import Follower from './Follower'
+
+const App = () => {
+  const { loading, data } = useFetch()
+  const [page, setPage] = useState(0)
+  const [followers, setFollowers] = useState([])
+
+  const handlePage = index => {
+    setPage(index)
+  }
+
+  const prevPage = () => {
+    setPage(oldPage => {
+      let prevPage = oldPage - 1
+      if (prevPage < 0) {
+        prevPage = data.length - 1
+      }
+      return prevPage
+    })
+  }
+
+  const nextPage = () => {
+    setPage(oldPage => {
+      let nextPage = oldPage + 1
+      if (nextPage > data.length - 1) {
+        nextPage = 0
+      }
+      return nextPage
+    })
+  }
+
+  useEffect(() => {
+    if (loading) return
+    setFollowers(data[page])
+  }, [loading, page])
+
+  return (
+    <main>
+      <div className="section-title">
+        <h1>{loading ? <div className="loader"><Loader type='spin' width={50} height={50} color='#1a6aa2' /></div> : 'pagination'}</h1>
+        <div className="underline"></div>
+      </div>
+      <section className="followers">
+        <div className="container">
+          {followers.map(follower => {
+            return <Follower key={follower.id} {...follower}/>
+          })}
+        </div>
+
+        {!loading && 
+          <div className="btn-container">
+            <button className='prev-btn' onClick={prevPage}>prev</button>
+            {data.map((item, index) => {
+              return (
+                <button key={index} className={`page-btn ${index === page && 'active-btn'}`} onClick={() => handlePage(index)}>
+                  {index + 1}
+                </button>
+              )
+            })}
+            <button className='next-btn' onClick={nextPage}>next</button>
+          </div>
+        }
+      </section>
+    </main>
+  )
+}
+
+export default App
